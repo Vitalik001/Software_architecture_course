979670b4f5f9d0bd4f643ffbe3be5caa04f0b5eb
Ipetrychkogit config --global user.email vitalii.petrychko@unidatalab.com <91615314+Vitalik001@users.noreply.github.com>
2024-03-10 11:23:07 +0200

    changed services naming

--- logging-service/main.py
+++ logging-service/main.py
@@ -1,15 +1,29 @@
+from typing import List
+
 from fastapi import FastAPI
 import logging
+from .models import Message
+
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = FastAPI(title="Logging service")
 
+messages_db = {}
 
-@app.get("/")
-async def get_logs():
+@app.get("/", response_model=List[str])
+async def get_messages():
 
-    result = "not implemented yet"
+    result = list(messages_db.values())
+    logging.info(f"Returned: {result}")
+    return result
+
+@app.post("/")
+async def post_message(message: Message):
+
+    logging.info(f"Got message with: {str(message)}")
+    messages_db[message.id] = message.text
+    result = 200
     logging.info(f"Returned: {result}")
     return result
--- messages-service/main.py
+++ messages-service/main.py
@@ -1,29 +1,15 @@
-from typing import List
-
 from fastapi import FastAPI
 import logging
-from .models import Message
-
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = FastAPI(title="Messages service")
 
-messages_db = {}
 
-@app.get("/", response_model=List[str])
+@app.get("/")
 async def get_messages():
 
-    result = list(messages_db.values())
-    logging.info(f"Returned: {result}")
-    return result
-
-@app.post("/")
-async def post_message(message: Message):
-
-    logging.info(f"Got message with: {str(message)}")
-    messages_db[message.id] = message.text
-    result = 200
+    result = "not implemented yet"
     logging.info(f"Returned: {result}")
     return result
