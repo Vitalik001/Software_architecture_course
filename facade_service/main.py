6ab6fb0fe344ac2130b1660b6dfc09cc639893d9
Ipetrychkogit config --global user.email vitalii.petrychko@unidatalab.com <91615314+Vitalik001@users.noreply.github.com>
2024-03-13 20:20:51 +0200

    final

--- facade_service/main.py
+++ facade_service/main.py
@@ -1,3 +1,5 @@
+import random
+
 from fastapi import FastAPI, HTTPException
 from .models import Message, PostMessage
 import logging
@@ -5,7 +7,7 @@ import requests
 import uuid
 
 messages_service_url = "http://messages-service:8080/"
-logging_service_url = "http://logging-service:8080/"
+logging_service_urls = ["http://logging-service-1:8080/", "http://logging-service-2:8080/", "http://logging-service-3:8080/"]
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
@@ -19,7 +21,7 @@ async def get_messages():
         messages_response = requests.get(messages_service_url)
         messages_response.raise_for_status()
         messages_data = messages_response.json()
-
+        logging_service_url = random.choice(logging_service_urls)
         logging_response = requests.get(logging_service_url)
         logging_response.raise_for_status()
         logging_data = logging_response.json()
@@ -40,10 +42,10 @@ async def post_message(message: Message):
     logging.info(f"Got message: {str(message)}")
     id = str(uuid.uuid4())
     data = PostMessage(id=id, text=message.text)
-
+    logging_service_url = random.choice(logging_service_urls)
     try:
         logging.info(f"Posting message: {str(data)}")
-        response = requests.post(messages_service_url, json=data.dict())
+        response = requests.post(logging_service_url, json=data.dict())
         response.raise_for_status()
         logging.info(f"Message posted successfully")
         return {"message": "Message posted successfully"}
--- logging-service/main.py
+++ logging-service/main.py
@@ -3,19 +3,30 @@ from typing import List
 from fastapi import FastAPI
 import logging
 from .models import Message
-
+import hazelcast
+import os
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = FastAPI(title="Logging service")
 
-messages_db = {}
+hazelcast_cluster_member = os.getenv("HAZELCAST_CLUSTER_MEMBER", "localhost:5701")
+
+client = hazelcast.HazelcastClient(
+    cluster_members=[
+        hazelcast_cluster_member
+    ],
+    cluster_name="hazel_claster"
+)
+
+distributed_map = client.get_map("distributed_map").blocking()
 
 @app.get("/", response_model=List[str])
 async def get_messages():
 
-    result = list(messages_db.values())
+    result = distributed_map.values()
+    print(result)
     logging.info(f"Returned: {result}")
     return result
 
@@ -23,7 +34,7 @@ async def get_messages():
 async def post_message(message: Message):
 
     logging.info(f"Got message with: {str(message)}")
-    messages_db[message.id] = message.text
+    distributed_map.set(message.id, message.text)
     result = 200
     logging.info(f"Returned: {result}")
     return result
