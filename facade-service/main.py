ac8d15021a981c790223a37ad989c83bc97d9562
Ipetrychkogit config --global user.email vitalii.petrychko@unidatalab.com <91615314+Vitalik001@users.noreply.github.com>
2024-03-25 18:01:55 +0200

    finished mq

--- facade-service/main.py
+++ facade-service/main.py
@@ -4,9 +4,10 @@ from fastapi import FastAPI, HTTPException
 from .models import Message, PostMessage
 import logging
 import requests
+import pika
 import uuid
 
-messages_service_url = "http://messages-service:8080/"
+messages_service_urls = ["http://messages-service-1:8080/", "http://messages-service-2:8080/"]
 logging_service_urls = ["http://logging-service-1:8080/", "http://logging-service-2:8080/", "http://logging-service-3:8080/"]
 
 logging.basicConfig(level=logging.INFO,
@@ -18,9 +19,11 @@ app = FastAPI(title="Facade service")
 @app.get("/")
 async def get_messages():
     try:
+        messages_service_url = random.choice(messages_service_urls)
         messages_response = requests.get(messages_service_url)
         messages_response.raise_for_status()
         messages_data = messages_response.json()
+
         logging_service_url = random.choice(logging_service_urls)
         logging_response = requests.get(logging_service_url)
         logging_response.raise_for_status()
@@ -47,8 +50,17 @@ async def post_message(message: Message):
         logging.info(f"Posting message: {str(data)}")
         response = requests.post(logging_service_url, json=data.dict())
         response.raise_for_status()
-        logging.info(f"Message posted successfully")
+        logging.info(f"{str(message)} posted successfully to logging service")
+
+        connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+        channel = connection.channel()
+        channel.queue_declare(queue='message_queue')
+        channel.basic_publish(exchange='', routing_key='message_queue', body=message.text)
+        logging.info(f"{str(message)} posted successfully to rabbitmq service")
+        connection.close()
+
         return {"message": "Message posted successfully"}
+
     except requests.exceptions.RequestException as e:
         logging.error(f"Failed to post message: {e}")
-        raise HTTPException(status_code=500, detail="Failed to post message to messages service")
+        raise HTTPException(status_code=500, detail="Failed to post message")
--- messages-service/main.py
+++ messages-service/main.py
@@ -1,15 +1,41 @@
 from fastapi import FastAPI
 import logging
+import pika
+from threading import Thread
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 app = FastAPI(title="Messages service")
+
+messages = []
+
+
+def rabbitmq_consumer():
+    global messages
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='message_queue')
+    #
+    def callback(ch, method, properties, body):
+        global messages
+        messages.append(body.decode())
+        logging.info(f"Received message: {body.decode()}")
+        logging.info(f"messages: {messages}")
+
+    channel.basic_consume(queue='message_queue', on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
+
+
+@app.on_event("startup")
+async def startup_event():
+    # Start the RabbitMQ consumer in a background thread
+    thread = Thread(target=rabbitmq_consumer)
+    thread.start()
 
 
 @app.get("/")
 async def get_messages():
-
-    result = "not implemented yet"
-    logging.info(f"Returned: {result}")
-    return result
+    global messages
+    logging.info(f"Returned messages: {messages}")
+    return messages
